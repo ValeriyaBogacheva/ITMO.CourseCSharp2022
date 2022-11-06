882c1e0639dbb19a5e3ae0a02cd42970195f999e
ValeriyaBogacheva <117594694+ValeriyaBogacheva@users.noreply.github.com>
2022-11-06 21:23:04 +0300

    Add files via upload

---
+++ ITMO.CourseCsarp2022.Lab3/WhatDay1/WhatDay.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatDay1
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    internal class WhatDay
+    {
+        private static int monthName;
+
+        static void Main( )
+        {
+            var DaysInMonths = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            Console.Write("Please enter a day number between 1 and 365: ");
+            int dayNum = int.Parse(Console.ReadLine());
+
+            int monthNum = 0;
+
+            foreach (int daysInMonth in DaysInMonths)
+            {
+                if (dayNum <= daysInMonth)
+                    break;
+
+                else
+                {
+                    dayNum -= daysInMonth;
+                    monthNum++;
+                }
+            }
+
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
+
+            Console.WriteLine("{0} {1}", dayNum, monthName);
+
+            Console.ReadKey(true);
+
+        }
+    }
+}
---
+++ ITMO.CourseCsarp2022.Lab3/WhatDay2/WhatDay2.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhatDay
+{
+    enum MonthName
+    {
+        January,
+        February,
+        March,
+        April,
+        May,
+        June,
+        July,
+        August,
+        September,
+        October,
+        November,
+        December
+    }
+
+    internal class WhatDay
+    {
+        static void Main( )
+        {
+            var DaysInMonths = new int[12] { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+
+            try
+            {
+                Console.Write("Please enter a day number between 1 and 365: ");
+                string line = Console.ReadLine( );
+                int dayNum = int.Parse(line);
+
+                if (dayNum < 1 || dayNum > 365)
+                {
+                    throw new ArgumentOutOfRangeException("Day out of range");
+                }
+
+                int monthNum = 0;
+
+                foreach (int daysInMonth in DaysInMonths)
+                {
+                    if (dayNum <= daysInMonth)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        dayNum -= daysInMonth;
+                        monthNum++;
+                    }
+                }
+                MonthName temp = (MonthName)monthNum;
+                string monthName = temp.ToString();
+
+                Console.WriteLine("{0} {1}", dayNum, monthName);
+            }
+            catch (Exception caught)
+            {
+                Console.WriteLine(caught);
+
+            }
+        }
+
+   
+
+
+    }
+}
